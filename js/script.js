8e6417372238d64e14b358d31c29b3f3fbc19e41
Luan <ita.luan12@gmail.com>
2023-02-09 18:30:19 -0300

    Implementing the functions to render the input boxes and the calculate button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,72 @@
+//function that is called when the page is opened
 function start() {
 
+    render();
+
+}
+
+//function that calls all other render functions
+function render() {
+
+    renderInputBoxes();
+    renderCalculateButton();
+    renderResults();
+
+}
+
+//function that render the calculate button
+function renderCalculateButton() {
+
+    var buttonDiv = document.querySelector('#button');
+    buttonDiv.innerHTML = '';
+    buttonDiv.appendChild(createCalculateButton());
+}
+
+//function that renders the input boxes
+function renderInputBoxes() {
+
+    var numbersDiv = document.querySelector("#numbers");
+    numbersDiv.innerHTML = '';
+    numbersDiv.appendChild(createFirstNumberInput());
+    numbersDiv.appendChild(createSecondNumberInput());
+    
+
+}
+//function that creates the calculate button
+function createCalculateButton() {
+
+    var button = document.createElement('button');
+    button.innerHTML = '';
+    button.textContent = 'Calculate';
+    button.addEventListener('click', calculate);
+    return button;
+
+}
+
+//function that creates the input box for the first number
+function createFirstNumberInput() {
+
+    var inputFirstNumber = document.createElement('input');
+    inputFirstNumber.innerHTML = '';
+    inputFirstNumber.type = 'number';
+    inputFirstNumber.placeholder = 'Enter number 1'
+    return inputFirstNumber;
+
+}
+
+//function that creates the input box for the second number
+function createSecondNumberInput() {
+
+    var inputSecondNumber = document.createElement('input');
+    inputSecondNumber.innerHTML = '';
+    inputSecondNumber.type = 'number';
+    inputSecondNumber.placeholder = 'Enter number 2'
+    return inputSecondNumber;
+
+}
+
+function calculate() {
+    console.log("macaco");
 }
 
 //function that calculates the sum of two given number
@@ -8,7 +75,7 @@ function sum(a, b) {
 }
 
 //function that returns the first given number subtracted by the second one
-function substract(a, b) {
+function subtract(a, b) {
     return a - b;
 }
 
@@ -41,11 +108,12 @@ function factorial(a) {
     return factorial
 }
 
-//function that 
+//function that calculates the percentage that the first number represents from the second one
 function percent(a, b) {
     return (a / b) * 100;
 }
 
+//function that calculates the average of two numbers
 function avg(a, b) {
     return (a + b) / 2;
 }
